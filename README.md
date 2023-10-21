c5fbfa619daba2d0f7d431819dffda39bcbaede6
Marcelo Bueno <marcelobueno25@hotmail.com>
2023-10-21 15:47:22 -0300

    melhoria no codigo em geral

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Moviepy-Python
 
+pip install -r requirements.txt
+
 https://www.youtube.com/watch?v=oOo1uENjXhI&ab_channel=EduardoMendes
--- main.py
+++ main.py
@@ -3,43 +3,16 @@ import random
 from moviepy.editor import (
     CompositeVideoClip, CompositeAudioClip,
     VideoFileClip, AudioFileClip, ImageClip, TextClip,
-    concatenate_videoclips, concatenate_audioclips
+    concatenate_videoclips, concatenate_audioclips, clips_array
 )
 from moviepy.video.tools.cuts import find_video_period
 
-video_file = 'video.mp4'
-audio_file = 'audio.mp3'
-image_file = 'image.png'
-output = 'output.mp4'
+video_file = 'assets/video.mp4'
+audio_file = 'assets/audio.mp3'
+image_file = 'assets/image.png'
+output = 'output/output.mp4'
 screensize = (1980, 1280)
-texts = "PEGOU FOGO! ESPERA AÍ! MERMÃO, o técnico da Seleção NÃO PODE... DEBATE FERVE sobre Diniz!"
-
-# txt_clip = TextClip(texts, 
-#                     fontsize=32,
-#                     color="white",
-#                     align='center',
-#                     method='caption',
-#                     size=(660, None))
-# txt_clip = CompositeVideoClip( [txt_clip.set_pos('center')],
-#                     size=screensize2)
-# video = video.resize(screensize1)
-    # video = video.rotate(180)
-# video = video.crop(x_center=1920 / 2, y_center=1080 / 4, width=1920, height=1080)
-
-# def createVideo(video, image, output):
-#     print('CREATE VIDEO')
-#     txt_clip = TextClip(texts, fontsize=70)
-#     txt_clip = txt_clip.set_pos('center').set_duration(5)
-    
-#     video = (VideoFileClip(video, audio=False).subclip(0))
-    
-#     image = ImageClip(image, duration=5)
-    
-#     print(str(video.duration))  
-    
-#     # clip = CompositeVideoClip([image, video, txt_clip])
-#     clip = CompositeVideoClip([image, video, txt_clip], size = screensize)
-#     clip.write_videofile('output.mp4')
+texts = "Automatizando edição de vídeos com Moviepy | Python"
 
 def createVideo(video_file, audio_file, image_file, output):
     
